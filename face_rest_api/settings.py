90bbb0c51b0d4005e7d51f80d4eef711935eea52
KARTHEEK <kartheek2000mike@gmail.com>
2019-03-14 23:13:55 +0530

    collect_static
    collect static dumped files to deploy_static

--- face_rest_api/settings.py
+++ face_rest_api/settings.py
@@ -133,8 +133,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-    '/media/',
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, "deploy_static")
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
